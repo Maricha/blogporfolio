283339fb75932bc2386382b78e0a30d30c3ed0e8
Maricha <maricha177@gazeta.pl>
2015-12-06 00:38:46 +0100

    polska

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < MailForm::Base
-  attribute :name, :validate => true
+  attribute :nazwa, :validate => true
   attribute :email, :validate => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  attribute :message, :validate => true
+  attribute :treść, :validate => true
   attribute :nickname, :captcha => true
 
 def headers
@@ -9,7 +9,7 @@ def headers
 
           :subject => "Contact Form",
            :to => "maricha177@gmail.com",
-           :from => %("#{name}" <#{email}>)
+           :from => %("#{nazwa}" <#{email}>)
         }
       end
 end
